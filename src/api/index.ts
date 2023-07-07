44fee2054bce01b7d97d038bcad21bf674c6f39d
janossik <marcinczaniecki97@gmail.com>
2023-07-07 14:31:33 +0200

    created dynamic imports routers

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
-import productRouter from '~/api/products/product.router';
-import tagRouter from '~/api/tags/tag.router';
-import categoryRouter from '~/api/categories/category.router';
-
+import fs from 'fs';
+import path from 'path';
 const api = Router();
 
-api.use('/products', productRouter);
-api.use('/tags', tagRouter);
-api.use('/categories', categoryRouter);
+for (const apiName of fs.readdirSync(__dirname)) {
+  const currentPath = path.join(__dirname, apiName);
+  if (!fs.statSync(currentPath).isDirectory()) continue;
+  for (const name of fs.readdirSync(currentPath)) {
+    if (name.includes('router.ts') || name.includes('router.js')) {
+      const router = require(path.join(currentPath, name))?.default;
+      router && api.use(`/${apiName}`, router);
+    }
+  }
+}
 
 export default api;
