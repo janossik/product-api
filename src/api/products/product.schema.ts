c4b3979ddce4ec1c8c3f5f6e442f80c4bb5e9362
janossik <marcinczaniecki97@gmail.com>
2023-07-08 00:03:47 +0200

    Separated schema on two piece, ProductCreated and ProductUpdated

--- src/api/products/product.schema.ts
+++ src/api/products/product.schema.ts
@@ -1,9 +1,28 @@
 import type { AllowedSchema } from 'express-json-validator-middleware';
 
-export const productSchema: AllowedSchema = {
+export const productCreatedSchema: AllowedSchema = {
   type: 'object',
   additionalProperties: false,
   required: ['name', 'author', 'code'],
+  properties: {
+    name: {
+      type: 'string',
+    },
+    author: {
+      type: 'string',
+    },
+    code: {
+      type: 'string',
+    },
+    description: {
+      type: 'string',
+    },
+  },
+};
+
+export const productUpdatedSchema: AllowedSchema = {
+  type: 'object',
+  additionalProperties: false,
   properties: {
     name: {
       type: 'string',
