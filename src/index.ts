1776a09540a383eaff24820174c4fc4c84550d0f
janossik <marcinczaniecki97@gmail.com>
2023-07-08 12:31:18 +0200

    Created two servers: development and production. The development server is the http server, The production server is the https server and extends type for the environment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import '~/config';
 import express from 'express';
-import ip from 'ip';
 import api from '~/api';
 import { errorHandler } from '~/error/error-handler';
+import { startDevelopmentServer, startProductionServer } from '~/utils/server.utils';
 
 const app = express();
 
@@ -10,20 +10,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', api);
-
 app.all('*', (req, res) => {
   const { method, url } = req;
   res.status(404).json({ message: `Path ${url} for method ${method} not found` });
 });
 
 app.use(errorHandler);
-app.listen(Number(process.env.PORT) || 3000, '0.0.0.0', () => {
-  console.log(`The application was started in ${process.env.NODE_ENV} mode at:\n`);
-  console.log(`     http://${ip.address()}:${process.env.PORT}`);
-  console.log(`     http://localhost:${process.env.PORT}`);
-});
 
-process.on('uncaughtException', async (err) => {
+startDevelopmentServer(app);
+startProductionServer(app);
+
+process.on('uncaughtException', function (err) {
   console.log('[Uncaught exception]:');
   console.log(err);
   console.log('--------------------------');
