8ed899494d11240ac7f2a3daf83711ec90f02b74
janossik <marcinczaniecki97@gmail.com>
2023-07-09 00:28:44 +0200

    Added relation logic "tags with products". For the product can add tags or products. And you can search product by tag

--- src/api/products/product.d.ts
+++ src/api/products/product.d.ts
@@ -1,10 +1,21 @@
+import { QueryNameLimitOffset } from '~/types/query';
+
 export interface Product {
   id: number;
   name: string;
   author: string;
   description: string;
+  tag_id?: number;
+  category_id?: number;
 }
 
 export type ProductCreated = Omit<Product, 'id'>;
 
 export type ProductUpdated = Partial<ProductCreated>;
+
+export interface QueryProduct extends QueryNameLimitOffset {
+  tag?: string;
+  category?: string;
+  withTags?: boolean;
+  withCategories?: boolean;
+}
--- src/api/products/product.service.ts
+++ src/api/products/product.service.ts
@@ -1,37 +1,100 @@
 import { Product } from '~/api/products/product.model';
-import { ProductCreated, ProductUpdated } from '~/api/products/product';
+import { ProductCreated, ProductUpdated, QueryProduct } from '~/api/products/product';
 import { QueryNameLimitOffset } from '~/types/query';
 
 export const productService = {
   async read(id: string | number) {
-    const product = await Product.query().findById(id);
+    const product = await Product.query().findById(id).throwIfNotFound({
+      message: 'Product not found',
+    });
     return {
       products: [product],
     };
   },
-  async readAll({ name, limit = 10, offset = 0 }: QueryNameLimitOffset) {
+
+  async readAllWithTagsAndCategories({ name, limit = 10, offset = 0 }: QueryNameLimitOffset) {
     if (name) {
-      const products = await Product.query().where('name', 'like', `%${name}%`).limit(Number(limit)).offset(Number(offset));
+      const products = await Product.query()
+        .withGraphFetched('[tags,categories]')
+        .where('name', 'like', `%${name}%`)
+        .limit(Number(limit))
+        .offset(Number(offset));
       return { products };
     }
 
-    const products = await Product.query().limit(Number(limit)).offset(Number(offset));
+    const products = await Product.query().withGraphFetched('[tags,categories]').limit(Number(limit)).offset(Number(offset));
     return {
       products,
     };
   },
-  async create(productCreated: ProductCreated) {
-    await Product.query().insert(productCreated);
+
+  async readAll({ name, limit = 10, offset = 0, tag, withTags, withCategories }: QueryProduct) {
+    if (tag) {
+      const products = await Product.query()
+        .withGraphFetched(`[tags ${withCategories ? ', categories' : ''}]`)
+        .modifiers({
+          tags(builder) {
+            builder.where('name', 'like', `%${tag}%`);
+          },
+        })
+        .limit(Number(limit))
+        .offset(Number(offset));
+      return { products };
+    }
+
+    if (name) {
+      const products = await Product.query()
+        .where('name', 'like', `%${name}%`)
+        .withGraphFetched(`[${withTags ? 'tags' : ''}${withCategories ? ', categories' : ''}]`)
+        .limit(Number(limit))
+        .offset(Number(offset));
+      return { products };
+    }
+
+    const products = await Product.query()
+      .withGraphFetched(`[${withTags ? 'tags' : ''}${withCategories ? ', categories' : ''}]`)
+      .limit(Number(limit))
+      .offset(Number(offset));
+
     return {
-      message: 'Product created',
+      products,
     };
   },
-  async update(id: string | number, productUpdated: ProductUpdated) {
-    await Product.query().findById(id).patch(productUpdated);
+
+  async create({ tag_id, category_id, ...productCreated }: ProductCreated) {
+    const result: Record<string, string> = { message: 'Product created' };
+    const product = await Product.query().insert(productCreated);
+
+    if (tag_id && !(await product.$relatedQuery('tags').where('tag_id', tag_id).resultSize())) {
+      await product.$relatedQuery('tags').relate(tag_id);
+    } else {
+      result.message += 'This tag exists.';
+    }
+
+    if (category_id && !(await product.$relatedQuery('categories').where('category_id', category_id).resultSize())) {
+      await product.$relatedQuery('categories').relate(category_id);
+    } else {
+      result.message += 'This category exists.';
+    }
+
+    return result;
+  },
+
+  async update(id: string | number, { tag_id, category_id, ...productUpdated }: ProductUpdated) {
+    const product = await Product.query().findById(id).throwIfNotFound({
+      message: 'Product not found',
+    });
+
+    await product.$query().patch(productUpdated);
+
+    if (tag_id) await product.$relatedQuery('tags').relate(tag_id);
+    if (category_id) await product.$relatedQuery('categories').relate(category_id);
+
     return {
       message: 'Product updated',
     };
   },
+
   async remove(id: string | number) {
     await Product.query().deleteById(id);
     return {
